3c815f92ec98971f6ac0c10daf336ff42d2fb934
shlomoShalem <s0552288304@gmail.com>
2024-11-14 11:50:27 +0200

    this is the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Nav from './components/Nav'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from './components/auths/Login'
+import Register from './components/auths/Register'
+import Control from './components/pages/Control'
 
 export default function App() {
   return (
     <div>
-      
+      <Nav />
+      <Routes>
+        <Route path='login' element={<Login/>} />
+        <Route path='register' element={<Register/>} />
+        <Route path='control' element={<Control/>} />
+        <Route path='/' element={<Navigate to={'login'}/>} />
+      </Routes>
     </div>
   )
 }
