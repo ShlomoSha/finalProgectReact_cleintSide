e024f388113342cc230e2873ff7c61f34997e9a2
shlomoShalem <s0552288304@gmail.com>
2024-11-14 16:26:50 +0200

    login components done

--- src/components/auths/Login.tsx
+++ src/components/auths/Login.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { RootState, useAppDispatch, useAppSelector } from '../../redux/stor'
+import { fetchLogin } from '../../redux/userSlice'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 export default function Login() {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const { user } = useAppSelector((state: RootState) => state.user)
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    if (!user?._id) return
+    navigate('/control')
+  }, [user])
+
+  const handleLogin = () => {
+    dispatch(fetchLogin({username, password}))
+  }
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <input type="text" placeholder='username' value={username} onChange={(e) => setUserName(e.target.value)} />
+      <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+      <p>no account yet? <NavLink to={'/register'}>join us</NavLink></p>
+    </div>
   )
 }
