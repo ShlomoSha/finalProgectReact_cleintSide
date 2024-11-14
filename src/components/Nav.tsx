83c2b14df55770b64b276428b32f84302c5fc51f
shlomoShalem <s0552288304@gmail.com>
2024-11-14 17:11:25 +0200

    nav componente are completed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,29 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { RootState, useAppDispatch, useAppSelector } from '../redux/stor';
+import userSlice from '../redux/userSlice';
 
 export default function Nav() {
+  const user = useAppSelector((state: RootState) => state.user);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    dispatch(userSlice.actions.logout());
+    localStorage.removeItem("Authorization");
+    navigate('/login')
+  }
+
   return (
     <div className='nav'>
-        <NavLink to={'/login'}>Login</NavLink>
-        <NavLink to={'/register'}>Register</NavLink>
-        <NavLink to={'/control'}>Control</NavLink>
+      {!user.user ? (
+        <>
+          <NavLink to={'/login'}>Login</NavLink>
+          <NavLink to={'/register'}>Register</NavLink>
+        </>
+        ) : (
+          <button onClick={handleLogOut}>Log out</button>
+        )}
+        
     </div>
   )
 }
