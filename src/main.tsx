de78f69ddd2125f12d0e4e82e25e587c14ce77e5
shlomoShalem <s0552288304@gmail.com>
2024-11-14 14:25:04 +0200

    stor done

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { io } from 'socket.io-client'
+import store from './redux/stor.ts'
+
+export const socket = io('http://localhost:3400')
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </BrowserRouter>
 )
